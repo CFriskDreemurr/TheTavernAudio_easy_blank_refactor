bb54e16992465ff9440f8fd7eab40be5642cd606
Frisk Dreemurr <capis1134@gmail.com>
2026-02-03 18:34:50 +0100

    lots of stuff :3

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -12,14 +12,7 @@ public class Interactor : MonoBehaviour
 
     public Transform InteractorSource;
     public float InteractRange;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
